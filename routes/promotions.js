a0d3416314fc46e2987de83fc2854dd29a1b7b82
TonyPurple <tony.vanoni@outlook.com>
2022-01-04 17:06:26 -0800

    adding isLoggedIn to all required routes, thinking there must be a better way to keep things DRY though

--- routes/promotions.js
+++ routes/promotions.js
@@ -1,7 +1,12 @@
 var router = require('express').Router();
 const promotionsCtrl = require('../controllers/promotions');
 
-router.get('/promotions/new', promotionsCtrl.new);
-router.post('/promotions', promotionsCtrl.create);
+router.get('/promotions/new', isLoggedIn, promotionsCtrl.new);
+router.post('/promotions', isLoggedIn, promotionsCtrl.create);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/auth/google");
+}
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,11 @@
 var router = require('express').Router();
 var reviewsCtrl = require('../controllers/reviews');
 
-router.post('/matches/:id/reviews', reviewsCtrl.create);
+router.post('/matches/:id/reviews', isLoggedIn, reviewsCtrl.create);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/auth/google");
+}
 
 module.exports = router;
